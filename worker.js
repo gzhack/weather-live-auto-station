5cf7b68209c5e8a4b006e077269cb3fceec18d69
Mr-Wiredancer <lijiahao90@gmail.com>
2015-05-14 14:46:57 +0800

    get data

--- worker.js
+++ worker.js
@@ -1,12 +1,24 @@
 var CronJob = require('cron').CronJob;
 var request = require('request');
 
-var job = new CronJob('00 * * * * 1-5', function() {
-  request.post({
-    url: 'https://hook.bearychat.com/=bw5Mw/incoming/9b00803217bbcc88077f2f2091da2b0d',
-    json: true,
-    body: {text:new Date()}
+var updateTime;
+
+var job = new CronJob('0 0,10,20,30,40,50 * * * *', function() {
+  request.get({
+    url: 'http://www.tqyb.com.cn/data/gzWeather/obtDatas.js',
+  }, function(err, res, body) {
+    var time = body.match(/\/\*@cdate\:(.*)\*\//)[1];
+    if (time===updateTime) return console.log('not updated yet');
+    else {
+      updateTime = time;
+      request.post({
+        url: 'https://hook.bearychat.com/=bw5Mw/incoming/9b00803217bbcc88077f2f2091da2b0d',
+        json: true,
+        body: {text:body}
+      });
+    }
   });
+
   /*
    * Runs every weekday (Monday through Friday)
    * at 11:30:00 AM. It does not run on Saturday
