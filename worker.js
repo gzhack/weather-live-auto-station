b14b7cb0dc539408a40d978442889b4ed6b05429
Mr-Wiredancer <lijiahao90@gmail.com>
2015-05-18 21:41:22 +0800

    t

--- worker.js
+++ worker.js
@@ -1,21 +1,36 @@
 var CronJob = require('cron').CronJob;
 var request = require('request');
+var _ = require('lodash');
+var fs = require('fs');
 
 var updateTime;
 
-var job = new CronJob('0 0,10,20,30,40,50 * * * *', function() {
+var isExist = function(str) {
+  try{
+    var fd = fs.openSync('./data/'+str, 'r');
+    fs.closeSync(fd);
+    return true;
+  }catch(e) {
+    return false;
+  }
+};
+
+var job = new CronJob('0 '+ _.range(0, 60, 1).join(',') +' * * * *', function() {
   request.get({
     url: 'http://www.tqyb.com.cn/data/gzWeather/obtDatas.js',
   }, function(err, res, body) {
     var time = body.match(/\/\*@cdate\:(.*)\*\//)[1];
-    if (time===updateTime) return console.log('not updated yet');
-    else {
+
+    if (!isExist(time)) {
       updateTime = time;
-      request.post({
-        url: 'https://hook.bearychat.com/=bw5Mw/incoming/9b00803217bbcc88077f2f2091da2b0d',
-        json: true,
-        body: {text:body}
-      });
+
+      fs.writeFile('./data/'+time, body, function(err) {
+        request.post({
+          url: 'https://hook.bearychat.com/=bw5Mw/incoming/9b00803217bbcc88077f2f2091da2b0d',
+          json: true,
+          body: {text:(err?('write file '+time+' failed'):(time+' saved'))}
+        });
+      })
     }
   });
 
